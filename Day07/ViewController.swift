53635c4f0712759db7667cf6e6e899c628eb627e
Linh <quanglinh95@gmail.com>
2016-10-29 10:31:14 +0700

    no message

---
+++ Day07/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Day07
+//
+//  Created by Admin on 10/27/16.
+//  Copyright © 2016 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var Keyboard: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
