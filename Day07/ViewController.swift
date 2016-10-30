d63966990de5cd55f3177815d65f7a8a06cca54b
Linh <quanglinh95@gmail.com>
2016-10-30 15:54:08 +0700

    no message

--- Day07/ViewController.swift
+++ Day07/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var Keyboard: UIView!
+    @IBOutlet weak var LeftKey: UIView!
+    @IBOutlet weak var RightKey: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
